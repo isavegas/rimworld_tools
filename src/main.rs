20bd5b553a1836177c4dace7cee959168bf51c1d
isavegas <isavegas2006@gmail.com>
2018-02-05 22:44:53 -0600

    Add

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,41 @@ mod userdata;
 
 
 use mod_info::load_mods;
+use userdata::load_modlist;
 use platform::platform::Platform;
 
+use std::env;
 
 fn main() {
-
-    let mods_result = load_mods(Platform::workshop_location().as_path());
-    if let Ok(mods) = mods_result {
-        mods.iter().for_each(|mod_info| println!("{:?}", mod_info))
-    } else {
-        println!("Error loading mods: {}", mods_result.unwrap_err());
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&String::from("mods")) {
+        let mods_result = load_mods(Platform::workshop_location().as_path());
+        if let Ok(mods) = mods_result {
+            mods.iter().for_each(|mod_info| println!("{:?}", mod_info))
+        } else {
+            println!("Error loading mods: {}", mods_result.unwrap_err());
+        }
     }
-
-    let mut userdata_location = Platform::install_location();
-    userdata_location.push("Mods");
-    let mods_result = load_mods(userdata_location.as_path());
-    if let Ok(mods) = mods_result {
-        mods.iter().for_each(|mod_info| println!("{:?}", mod_info))
-    } else {
-        println!("Error loading mods: {}", mods_result.unwrap_err());
+    if args.contains(&String::from("mods")) {
+        let mut install_location = Platform::install_location();
+        install_location.push("Mods");
+        let mods_result = load_mods(install_location.as_path());
+        if let Ok(mods) = mods_result {
+            mods.iter().for_each(|mod_info| println!("{:?}", mod_info))
+        } else {
+            println!("Error loading mods: {}", mods_result.unwrap_err());
+        }
+    }
+    if args.contains(&String::from("modlist")) {
+        let mut userdata_location = Platform::userdata_location();
+        userdata_location.push("Config");
+        userdata_location.push("ModsConfig.xml");
+        let modslist_result = load_modlist(userdata_location.as_path());
+        if let Ok(modlist) = modslist_result {
+            println!("Active mods: {:?}", modlist);
+        } else {
+            println!("Error loading modlist: {}", modslist_result.unwrap_err())
+        }
     }
 
     // May be unstable.
